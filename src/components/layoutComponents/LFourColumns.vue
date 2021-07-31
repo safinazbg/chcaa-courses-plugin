87cdae7a6f428102061b75ecafac130c10a1a0a4
safinazbg <safinazbuyukguzel@gmail.com>
2021-07-31 15:02:32 +0200

    create layout components
    very useful with slots

---
+++ src/components/layoutComponents/LFourColumns.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="columns">
+
+    <slot name="gridItems"></slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+name: "LFourColumns"
+}
+</script>
+
+<style scoped>
+.columns {
+  display: grid;
+  width: 100%;
+  padding: 1rem;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 1rem;
+}
+</style>
---
+++ src/components/layoutComponents/LTwoColumns.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="columns">
+
+    <slot name="items"></slot>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LTwoColumns"
+}
+</script>
+
+<style scoped>
+  .columns {
+    display: grid;
+    width: 100%;
+    padding: 1rem;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
+  }
+
+
+</style>
