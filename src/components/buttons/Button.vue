bc45bb9288bbc0633b20224ce05ca69d37fe795d
safinazbg <safinazbuyukguzel@gmail.com>
2021-07-31 15:02:49 +0200

    Create Button.vue

---
+++ src/components/buttons/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="button uk-button uk-button-default">{{name}}</button>
+</template>
+
+<script>
+export default {
+name: "Button",
+  props: ["name"]
+}
+</script>
+
+<style scoped>
+.button {
+  border-radius: 5px;
+}
+</style>
