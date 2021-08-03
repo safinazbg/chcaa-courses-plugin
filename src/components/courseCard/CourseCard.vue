d8f5934d982f8f0938c48a3d2e396cee40bdfdd8
safinazbg <safinazbuyukguzel@gmail.com>
2021-08-03 12:34:35 +0200

    Update CourseCard.vue

--- src/components/courseCard/CourseCard.vue
+++ src/components/courseCard/CourseCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="uk-card uk-card-default uk-card-hover uk-card-body" :style="{backgroundColor: color}">
-      <span class="uk-badge uk-align-right uk-margin-remove">{{ course.level }}</span>
+      <CourseLabel class="uk-align-right">{{course.level}}</CourseLabel>
       <h3 class="uk-card-title">{{ course.title }}</h3>
       <p class="uk-text-left">{{ snippet }}</p>
       <div class="uk-padding-small">
@@ -10,13 +10,18 @@
 </template>
 
 <script>
+import CourseLabel from "../CourseLabel";
+
 import Button from "../buttons/Button";
 import {computed} from "@vue/reactivity";
 
 export default {
   name: "CourseCard",
   props: ['course', 'color'],
-  components: { Button},
+  components: {
+    Button,
+    CourseLabel
+  },
 
   setup(props) {
     const snippet = computed(() => {
