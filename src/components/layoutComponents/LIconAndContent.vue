c0d7fcf06db19dc3af1992ee9610308f7b44fbad
safinazbg <safinazbuyukguzel@gmail.com>
2021-08-03 12:35:12 +0200

    Create LIconAndContent.vue

---
+++ src/components/layoutComponents/LIconAndContent.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="iconAndContent">
+  <div class="icon">
+    <span class="uk-icon" uk-icon="check"></span>
+  </div>
+  <div class="content">
+    <slot name="content">
+
+    </slot>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "LIconAndContent"
+}
+</script>
+
+<style scoped>
+.iconAndContent {
+  display: grid;
+  grid-template-columns: 1rem 1fr;
+  grid-gap: 1rem;
+}
+
+
+</style>
