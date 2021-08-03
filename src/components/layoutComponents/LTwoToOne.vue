e69dd3ee1874219ced9defeeab26ae42aad1fabf
safinazbg <safinazbuyukguzel@gmail.com>
2021-08-03 12:35:28 +0200

    Create LTwoToOne.vue

---
+++ src/components/layoutComponents/LTwoToOne.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="twoToOne">
+  <div>
+    <slot name="two">
+
+    </slot>
+  </div>
+  <div>
+    <slot name="one">
+
+    </slot>
+  </div>
+</div>
+  <slot name="test">
+
+  </slot>
+</template>
+
+<script>
+export default {
+  name: "LTwoToOne"
+}
+</script>
+
+<style scoped>
+.twoToOne {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+}
+</style>
