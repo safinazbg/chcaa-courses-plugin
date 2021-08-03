9f3a38a0da160711b43a7212f5fefb0cfa6f87cc
safinazbg <safinazbuyukguzel@gmail.com>
2021-08-03 12:34:48 +0200

    Update CourseLabel.vue

--- src/components/CourseLabel.vue
+++ src/components/CourseLabel.vue
@@ -1,13 +1,16 @@
 <template>
-
+  <span class=" label uk-label">Default</span>
 </template>
 
 <script>
 export default {
-name: "CourseLabel"
+name: "CourseLabel",
+  props: ['labelName']
 }
 </script>
 
 <style scoped>
-
+.label {
+  border-radius: 10px;
+}
 </style>
