0296f941816b5ea75c3a0f50c5c6e9f9e179aae2
safinazbg <safinazbuyukguzel@gmail.com>
2021-08-06 09:48:53 +0200

    Update CourseCardWithImage.vue

--- src/components/courseCard/CourseCardWithImage.vue
+++ src/components/courseCard/CourseCardWithImage.vue
@@ -1,31 +1,32 @@
 <template>
-    <div class="personCard uk-card uk-card-default">
-      <div class="uk-card-media-left uk-padding-remove-left">
-        <img src="../assets/logo.png" width="240" >
-      </div>
-      <div class="uk-card-body uk-text-left">
-        <div class="uk-text-emphasis">Role</div>
-        <div class="uk-text-bold">Name</div>
-        <div class="uk-text-italic">Affiliation</div>
-        <hr class="uk-divider-small">
-        <div>Email</div>
-        <div>Tel</div>
-      </div>
-    </div>
+<LTwoColumns>
+  <template v-slot:items>
+    <img src="../../assets/logo.png">
+    <CourseCard :course="course"></CourseCard>
+  </template>
+</LTwoColumns>
+
 </template>
 
 <script>
+import CourseCard from "./CourseCard";
+import LTwoColumns from "../layoutComponents/LTwoColumns";
 export default {
-name: "CourseCardWithImage"
+name: "CourseCardWithImage",
+  components:{
+    LTwoColumns,
+    CourseCard,
+  },
+  props: ["course"]
 }
 </script>
 
 <style scoped>
-.personCard {
-  width: 500px;
-  display: grid;
-  grid-template-columns: 240px 1fr;
-}
+/*.personCard {*/
+/*  width: 500px;*/
+/*  display: grid;*/
+/*  grid-template-columns: 240px 1fr;*/
+/*}*/
 /*.verticallyCentered {*/
 /*  display: flex;*/
 /*  flex-direction: column;*/
