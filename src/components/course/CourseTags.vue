5352a24112424f71f6a9840161f34231a4f1299f
safinazbg <safinazbuyukguzel@gmail.com>
2021-08-06 09:48:13 +0200

    Create CourseTags.vue

---
+++ src/components/course/CourseTags.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+      class="uk-label courseTags"
+      v-for="tag in courseTags"
+      :key="tag"
+  >
+    {{tag}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CourseTags",
+    props: {
+      courseTags: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
