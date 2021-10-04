36b62505edf88c576551af7e6c27bacbf93c787d
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2021-10-04 11:02:07 +0200

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="gridLayout">
+    <div class="navbar uk-padding-small">
+        <ul class="uk-iconnav uk-iconnav-vertical">
+          <li @click="$router.push('/')">
+            <a uk-icon="icon: home"></a>
+          </li>
+          <li @click="$router.push('/courses')"><a uk-icon="icon: file-edit"></a></li>
+          <li @click="$router.push('/modules')"><a uk-icon="icon: user"></a></li>
+          <li><a uk-icon="icon: settings"></a></li>
+        </ul>
+      </div>
+      <div id="body" class="uk-padding">
+        <router-view></router-view>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+#gridLayout {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 50px 1fr;
+}
+.navbar {
+  display: flex;
+  justify-content: center;
+  background-color: #eeeeee;
+}
+
+#nav svg {
+    color: black;
+  }
+</style>
