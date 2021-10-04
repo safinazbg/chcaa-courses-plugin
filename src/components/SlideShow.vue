deb88657f45f1372b81182387bb76d6360b2850a
Safinaz Büyükgüzel <42682032+safinazbg@users.noreply.github.com>
2021-10-04 11:01:57 +0200

    try carousel with uikit
    and it does not work

--- src/components/SlideShow.vue
+++ src/components/SlideShow.vue
@@ -1,30 +1,30 @@
 <template>
 <div class="slideShow">
   <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow>
+    <ul class="uk-slideshow-items" v-for="slide in slides" :key="slide">
+      <li>
+        <img :src="require(`@/assets/${slide}.png`)"uk-cover>
+      </li>
 
-    <ul class="uk-slideshow-items">
-      <li>
-        <img src="../assets/git.png"  uk-cover>
-      </li>
-      <li>
-        <img src="../assets/jupyter.png"  uk-cover>
-      </li>
-      <li>
-        <img src="../assets/logo.png" uk-cover>
-      </li>
     </ul>
-
     <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
     <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
-
   </div>
 </div>
 
 </template>
 
 <script>
+import {ref} from "@vue/reactivity";
+
 export default {
-  name: "SlideShow"
+  name: "SlideShow",
+  setup() {
+    const slides = ref(['git', 'jupyter', 'logo'])
+    return {
+      slide
+    }
+  }
 }
 </script>
 
